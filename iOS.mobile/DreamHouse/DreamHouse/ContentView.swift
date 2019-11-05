ebb0bbf467859b137f999be85948c2c3bd00f8d1
leodotng <josh@leo.ng>
2019-11-04 19:20:41 -0700

    initial commit

---
+++ iOS.mobile/DreamHouse/DreamHouse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DreamHouse
+//
+//  Created by Josh Leong on 11/4/19.
+//  Copyright © 2019 Josh Leong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
